18ec02ba62b11e535bc369345d74f91343a3e5d1
Mikolaj Kedzierski <mikked@poczta.onet.pl>
2019-10-29 08:06:27 +0100

    removed unused import

--- src/main/java/pl/sdacademy/todolist/service/ApplicationUserDetailService.java
+++ src/main/java/pl/sdacademy/todolist/service/ApplicationUserDetailService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.sdacademy.todolist.entity.Role;
 import pl.sdacademy.todolist.entity.User;
